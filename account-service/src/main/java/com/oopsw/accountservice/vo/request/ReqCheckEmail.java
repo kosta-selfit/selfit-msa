4eff0a4e1ea64949eda2c84805474c147fa048f3
kokoko12334 <gnjgnjgnj1@gmail.com>
2025-07-04 14:39:54 +0900

    feat: Account에 쓰는 VO추가

---
+++ account-service/src/main/java/com/oopsw/accountservice/vo/request/ReqCheckEmail.java
@@ -0,0 +1,8 @@
+package com.oopsw.accountservice.vo.request;
+
+import lombok.Data;
+
+@Data
+public class ReqCheckEmail {
+	private String email;
+}
---
+++ account-service/src/main/java/com/oopsw/accountservice/vo/request/ReqCheckNickname.java
@@ -0,0 +1,8 @@
+package com.oopsw.accountservice.vo.request;
+
+import lombok.Data;
+
+@Data
+public class ReqCheckNickname {
+	private String nickname;
+}
