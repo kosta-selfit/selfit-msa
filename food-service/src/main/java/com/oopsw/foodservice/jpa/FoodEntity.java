6ea9adf83fb971cac103d9090b36033f5d3a308f
phs <liaparkel@gmail.com>
2025-07-07 18:12:09 +0900

    feat: FoodEntity 수정

--- food-service/src/main/java/com/oopsw/foodservice/jpa/FoodEntity.java
+++ food-service/src/main/java/com/oopsw/foodservice/jpa/FoodEntity.java
@@ -1,6 +1,13 @@
 package com.oopsw.foodservice.jpa;
 
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,6 +20,24 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name = "food")
-public class FoodEntity {
+public class FoodEntity implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
+	@Column(nullable = false, unique = true)
+	private String foodNoteId;
+	@Column(nullable = false)
+	private Date intakeDate;
+	@Column(nullable = false)
+	private String foodName;
+	@Column(nullable = false)
+	private Float intake;
+	@Column(nullable = false)
+	private Float intakeKcal;
+	@Column(nullable = false)
+	private int unitKcal;
+
+	@Column(nullable = false)
+	private String memberId;
 }
