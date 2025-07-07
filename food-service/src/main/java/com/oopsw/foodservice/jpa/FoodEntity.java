ebb750e0eb9848dc5e792a6da0bf8beaa0de937f
phs <liaparkel@gmail.com>
2025-07-07 14:25:40 +0900

    feat: FoodEntity 수정

--- food-service/src/main/java/com/oopsw/foodservice/jpa/FoodEntity.java
+++ food-service/src/main/java/com/oopsw/foodservice/jpa/FoodEntity.java
@@ -1,4 +1,18 @@
 package com.oopsw.foodservice.jpa;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "food")
 public class FoodEntity {
+
 }
