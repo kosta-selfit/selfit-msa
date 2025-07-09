bf7bd729cc783a3955cf585a1be76d9457f1583a
YunhoYeom <whatever.u.r1103@gmail.com>
2025-07-09 16:23:02 +0900

    feat: exercise-service 완성

--- exercise-service/src/main/java/com/oopsw/exerciseservice/dto/ExerciseDto.java
+++ exercise-service/src/main/java/com/oopsw/exerciseservice/dto/ExerciseDto.java
@@ -2,6 +2,8 @@ package com.oopsw.exerciseservice.dto;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,16 +18,18 @@ public class ExerciseDto {
 	private String memberId;
 	private String exerciseId;
 	private String exerciseDate;
+	@JsonProperty("운동명")
 	private String exerciseName;
 	private int exerciseMin;
 	private float exerciseKcal;
+	@JsonProperty("단위체중당에너지소비량")
 	private float met;
-	private String keyword;
 	private float exerciseSum;
 	private String year;
-
+	private int newMin;
 
 	private int pageNo;
 	private int numOfRows;
-	private int newMin;
+	private String keyword;
+
 }
